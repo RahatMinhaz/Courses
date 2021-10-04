a4f8f870be072883da8e3ee150bec8bad9c62f8c
RahatMinhaz <minhaz7591@gmail.com>
2021-10-04 23:31:27 +0600

    button added

--- src/components/Subject/Subject.js
+++ src/components/Subject/Subject.js
@@ -9,6 +9,7 @@ const Subject = (props) => {
             <h4>Course Fee: {price} tk</h4>
             <h4>Course Duration: {duration} months</h4>
             <h3>Who's Eligible: {level}</h3>
+            <button className = "btn btn-primary p-3 mt-5">Buy This Course</button>
         </div>
     );
 };
